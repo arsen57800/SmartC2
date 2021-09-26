f2648fc9061c1fac64ee8b1bb125b542d3783df2
arsen <arsen57800@gmail.com>
2021-09-27 01:38:19 +0200

    new project

---
+++ contracts/Election.sol
@@ -0,0 +1,15 @@
+pragma solidity  ^0.5.16;
+
+/**
+ * The contractName contract does this and that...
+ */
+contract Election {
+
+	string public candidate;
+
+
+	function Election1() public{
+		candidate = "Candidate 1";
+	}
+	
+}
